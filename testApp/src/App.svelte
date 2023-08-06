a2ecaf3b257b1f88639be81e265bc4ef68e1a870
florisweb <floris@florisweb.dev>
2023-08-06 16:53:04 +0200

    Started converting to svelve, converted the dropDown-UI-component.

---
+++ testApp/src/App.svelte
@@ -0,0 +1,17 @@
+<script>
+	import DropDown from "./UI/dropDown.svelte";
+	
+	let options = [];
+
+	for (let x = 0; x < 50; x++)
+	{
+		options.push({
+			value: x,
+			contentHTML: 'test ' + x
+		});	
+	}
+</script>
+
+<main>
+	<DropDown options={options} on:change={(_event) => console.log(_event.detail)}></DropDown>
+</main>
---
+++ testApp/src/UI/dropDown.svelte
@@ -0,0 +1,158 @@
+<script>
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+
+	export let customClass;
+	export let isOpen = false;
+	export let options;
+
+	let selectedOption = {
+		value: false,
+		contentHTML: 'No option selected'
+	}
+
+	$: dispatch('change', selectedOption.value);
+	$: if (isOpen) updatePanelPosition();
+
+	
+	let optionPanel;
+	let openAbove = true;
+	let top = 0;
+	let maxHeight = 500;
+
+	function updatePanelPosition() {
+		console.log('try update');
+		if (!optionPanel) return;
+		const screenMargin = 20;
+		
+		let box = optionPanel.getBoundingClientRect();
+		let panelHeight = optionPanel.offsetHeight;
+	
+		let topSpaceLeft = box.top - panelHeight - screenMargin;
+		let bottomSpaceLeft = window.innerHeight - (box.top + panelHeight) - screenMargin;
+
+		let biggestSpace = Math.max(topSpaceLeft, bottomSpaceLeft);
+		maxHeight = panelHeight + biggestSpace;
+		openAbove = topSpaceLeft === biggestSpace;	
+	}
+</script>
+
+
+<div class={"wrapper " + (customClass ? customClass : '') + (isOpen ? ' optionPanelOpen' : '')}>
+	<!-- svelte-ignore a11y-click-events-have-key-events -->
+	<div class='button' on:click={() => isOpen = !isOpen}>
+		<img src='images/dropDownIconDark.png' alt="Dropdown icon." class='dropDownIcon'>
+		<div class='contentHolder'>{selectedOption.contentHTML}</div>
+	</div>
+	<div 
+		bind:this={optionPanel}
+		class={'optionPanel' + (openAbove ? ' openAbove' : '')} 
+		style={'top: ' + top + 'px; max-height: ' + maxHeight + 'px'}
+	>
+		{#each options as option}
+			<!-- svelte-ignore a11y-click-events-have-key-events -->
+			<div class='option' on:click={() => {selectedOption = option; isOpen = false}}>{option.contentHTML}</div>
+		{/each}
+	</div>
+</div>
+
+
+
+<style>
+	.wrapper {
+		position: relative;
+		margin: 200px;
+	}
+
+
+	/* BUTTON */
+	.wrapper .button {
+		position: relative;
+		display: flex;
+		
+		width: auto;
+		height: 35px;
+		padding: 7.5px;
+
+		font-size: 12px;
+		background: #fff;
+		box-shadow: 10px 10px 30px 10px rgba(0, 0, 0, .02);
+		border: 1px solid #eee;
+
+		cursor: pointer;
+	}
+
+	.wrapper .button .dropDownIcon {
+		position: relative;
+		height: 12px;
+		width: auto;
+		opacity: .5;
+		margin: 4px;
+		transition: transform .3s;
+		transform: rotateZ(-90deg);
+	}
+	.wrapper.optionPanelOpen .button .dropDownIcon {
+		transform: rotateZ(0);
+	}
+	.wrapper.optionPanelOpen.openAbove .button .dropDownIcon {
+		transform: rotateZ(-180deg);
+	}
+
+	.wrapper .button .contentHolder { 
+		position: relative;
+		margin-left: 5px;
+		height: 20px;
+		line-height: 20px;
+		width: auto;
+		overflow: hidden;
+	}
+
+
+
+	/* OPTIONPANEL */
+	.wrapper .optionPanel {
+		position: absolute;
+		min-width: 120px;
+		height: auto;
+
+		padding: 5px 20px;
+
+		box-shadow: 10px 10px 30px 10px rgba(0, 0, 0, .05);
+		border: 1px solid #eee;
+
+		z-index: 1000;
+		background: #fff;
+
+		transition: opacity .3s, margin-top .3s;
+
+		margin-top: 0;
+		overflow: auto;
+	}
+
+	.wrapper:not(.optionPanelOpen) .optionPanel {
+		pointer-events: none;
+		opacity: 0;
+		margin-top: -20px;
+	}
+	.wrapper:not(.optionPanelOpen).openAbove .optionPanel {
+		margin-top: 20px;
+	}
+
+	.wrapper.openAbove .optionPanel {
+		transform: translateY(calc(-100% - 35px));
+	}
+
+
+	/* OPTION */
+	.option {
+		cursor: pointer;
+	}
+	.option > a {
+		position: relative;
+		height: 20px;
+		line-height: 20px;
+		font-size: 13px;
+		color: #444;
+		margin-left: -5px;
+	}
+</style>
